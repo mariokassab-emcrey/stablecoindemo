d902c8d0ef784ddc41e9913f903e9e778d821ede
mariokassab-emcrey <mario.kassab@emcrey.com>
2025-07-08 12:03:18 +0300

    Update AccountBookdialog.js
    resolving the conflicts

--- src/AccountBookdialog.js
+++ src/AccountBookdialog.js
@@ -1,58 +1,61 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Table, Modal, Button, Row, Col, Form } from "react-bootstrap";
+import React, { useRef,useState,useEffect } from 'react';
+import { Table,Modal, Button, Row, Col, Form } from "react-bootstrap";
 import './App.css';
-import { collection, getDocs, addDoc, deleteDoc, doc } from "firebase/firestore";
+import { collection, getDocs,addDoc,deleteDoc,doc } from "firebase/firestore";
 import { db } from "./firebaseConfig";
-import { Check, Copy, Plus, SquareX, Trash2 } from 'lucide-react';
+import { Check,Copy,Plus,SquareX,Trash2,X } from 'lucide-react';
 function AccountBookDialog(props) {
   const dialogRef = useRef(null);
   const [AccountBook, setAccountBook] = useState([]);
-
+  const [IsFormVisible,setIsFormVisible] = useState(["false"]);
+ 
   const openDialog = () => {
-
-
+  
+           
     dialogRef.current.showModal(); // Opens the dialog
   };
 
   const closeDialog = () => {
     dialogRef.current.close(); // Closes the dialog
   };
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const newAccount = {
-
-      Address: event.target.Address.value,
-      Name: event.target.Name.value,
-
-      MainAccount: props.mainAccount
-    };
-    try {
-
+async function handleSubmit(event) {
+  event.preventDefault();
+  const newAccount = {
+    
+    Address: event.target.Address.value,
+    Name: event.target.Name.value,
+    
+    MainAccount : props.mainAccount
+  };
+  try {
+      
 
       await addDoc(collection(db, "Book"), newAccount);
       alert("Account Added successfully!");
-
+      setIsFormVisible("false")
       // ✅ Redirect back to menu after successful order
       window.location.href = "/";
     } catch (error) {
       console.error("Error placing order: ", error);
       alert("Failed to place order.");
     }
+  
+  // setAccountBook([...AccountBook, newAccount]);
 
-  }
+}
 
-  useEffect(() => {
+useEffect(() => {
     const fetchMenu = async () => {
       const menuCollection = await getDocs(collection(db, "Book"));
-      console.log("menuCollection.docs" + menuCollection.docs[0].id)
+       console.log("menuCollection.docs"+menuCollection.docs[0].id)
       const Addresses = menuCollection.docs.map((doc) => ({
-        ID: doc.id,
+       ID:doc.id,
         Address: doc.data().Address,
         Amount: doc.data().Amount,
         Name: doc.data().Name,
-        MainAccount: doc.data().MainAccount,
+        MainAccount:doc.data().MainAccount,
       }));
-
+     
       setAccountBook(Addresses);
     };
     fetchMenu();
@@ -64,23 +67,23 @@ function AccountBookDialog(props) {
         Address Book
       </button>
       <dialog ref={dialogRef}>
-        <div class="container mx-auto px-4 py-2 bg-panel shadow-lg noBorder border-gray-700">
+        <div class="container mx-auto px-4 py-2 bg-panel shadow-lg border border-gray-700">
 
-          <div class="flex justify-between items-center mb-8">
-            <div class="flex items-center gap-4">
-              <h1 class="text-3xl font-bold text-white">Addresses</h1>
-            </div>
-            <div class="text-right">
-              <SquareX class="text-gray-300 hover:text-secondary p-1 hover:bg-gray-700 transition-colors w-7 h-7" onClick={closeDialog} />
-            </div>
+                            <div class="flex justify-between items-center mb-8">
+                                <div class="flex items-center gap-4">
+                                    <h1 class="text-3xl font-bold text-white">Accounts</h1>
+                                </div>
+                                <div class="text-right"> 
+                                <SquareX class="text-gray-300 hover:text-secondary p-1 hover:bg-gray-700 transition-colors w-7 h-7" onClick={closeDialog} />
+                                </div>
+                             
+                            </div>
 
-          </div>
-
-          <div class="bg-card rounded-lg shadow-lg px-6 py-1 noBorder border-gray-700 mb-6">
-            <div class="flex justify-between items-center">
-              <div class="flex gap-8">
-
-                <Table className="mt-4" hover size="sm" style={{ color: 'white' }}>
+                            <div class="bg-card rounded-lg shadow-lg px-6 py-1 border border-gray-700 mb-6">
+                                <div class="flex justify-between items-center">
+                                    <div class="flex gap-8">
+                                       
+                                        <Table className="mt-4" hover size="sm" style={{ color: 'white' }}>
                   <thead>
                     <tr>
                     </tr>
@@ -90,33 +93,33 @@ function AccountBookDialog(props) {
                       <tbody>
                         {AccountBook.map(
                           (account, index) => (
-                            account.MainAccount === props.mainAccount ?
-                              <tr key={index}>
-                                <td>
-                                  <div class="flex items-center gap-4 p-1 hover:bg-gray-700 rounded">
-                                    <div class="w-10 h-10 rounded-full border-2 border-green-700 flex items-center justify-center" onclick="">
-                                      <Check class="w-5 h-5 text-green-700 " />
-                                    </div>
+                            account.MainAccount === props.mainAccount ? 
+                            <tr key={index}>
+                              <td>
+                                <div class="flex items-center gap-4 p-1 hover:bg-gray-700 rounded">
+                                  <div class="w-10 h-10 rounded-full border-2 border-green-700 flex items-center justify-center" >
+                                    <Check class="w-5 h-5 text-green-700 " />
                                   </div>
-                                </td>
+                                </div>
+                              </td>
 
-                                <td className="td-home">
-                                  <div class="flex flex-col">
-                                    <p class="text-lg font-bold">{account.Name}</p>
-                                    <div class="display: flex gap-1 text-gray-300">
-                                      {props.truncateEthAddress(account.Address)}
-                                      <button class="focus:outline-none" onClick={() => props.copyTextToClipboard(account.Address)} >
-                                        <Copy size={12} />
-                                      </button>
-                                    </div>
+                              <td className="td-home">
+                                <div class="flex flex-col">
+                                  <p class="text-lg font-bold">{account.Name}</p>
+                                  <div class="display: flex gap-1 text-gray-300">
+                                    {props.truncateEthAddress(account.Address)}
+                                    <button class="focus:outline-none" onClick={() => props.copyTextToClipboard(account.Address)} >
+                                      <Copy size={12} />
+                                    </button>
                                   </div>
-                                </td>
+                                </div>
+                              </td>
 
-                                <td style={{ width: '200px' }}>
-                                  <Row style={{ paddingLeft: '25px' }}>
+                              <td style={{ width: '200px' }}>
+                                <Row style={{ paddingLeft: '25px' }}>
 
-                                    {props.allowPay === 'true'
-                                      ? (
+                                  {props.allowPay === 'true' 
+                                    ? (
                                         <button class="text-sm font-medium text-button px-3 py-1 rounded hover:bg-panel bg-gray-700 transition-colors border border-button focus:outline-none"
                                           onClick={() => {
                                             props.fillAccountDetails(account)
@@ -124,33 +127,90 @@ function AccountBookDialog(props) {
                                           }}>
                                           Pay
                                         </button>
-                                      )
-                                      : (<div />)}
+                                    )
+                                    :(<div/>)}
 
-                                    <div style={{ width: '10px', height: '10px' }}></div>
-                                    <Trash2 class="text-gray-300 hover:text-secondary p-1 rounded-full hover:bg-gray-700 transition-colors"
-                                      onClick={async () => {
-                                        await deleteDoc(doc(db, "Book", account.ID));
-                                        setAccountBook(AccountBook.filter((item) => item.ID !== account.ID));
-                                      }}>
-                                    </Trash2>
-                                  </Row>
-                                </td>
-                              </tr> : null
+                                  <div style={{ width: '10px', height: '10px' }}></div>
+                                  <Trash2 class="text-gray-300 hover:text-secondary p-1 rounded-full hover:bg-gray-700 transition-colors"
+                                    onClick={async () => {
+                                      await deleteDoc(doc(db, "Book", account.ID));
+                                      setAccountBook(AccountBook.filter((item) => item.ID !== account.ID));
+                                    }}>
+                                  </Trash2>
+                                </Row>
+                              </td>
+                            </tr>:null
                           )
                         )}
                         <tr>
                           <td>
-                            <div class="flex items-center gap-4 p-1 hover:bg-gray-700 rounded">
+                            {IsFormVisible === "true" ? null : <div class="flex items-center gap-4 p-1 hover:bg-gray-700 rounded">
                               <div class="group w-10 h-10 rounded-full border-2 border-dashed border-gray-500 flex items-center justify-center hover:border-secondary" onclick="">
-                                <Plus class="w-5 h-5 text-gray-500 group-hover:text-secondary" />
+                               <button> <Plus onClick={()=>setIsFormVisible("true")} class="w-5 h-5 text-gray-500 group-hover:text-secondary" /></button>
                               </div>
-                            </div>
+                            </div>}
+                            
                           </td>
                           <td>
-                            <div class=" flex-1 min-w-0">
-                              <h3 class="font-medium text-gray-400 group-hover:text-secondary">Add New Address</h3>
-                            </div>
+                            {IsFormVisible === 'true' ? <Form style={{color : 'white'}} onSubmit={handleSubmit}>
+                                            <Row>
+                                                <Col>
+                                                <Form.Group controlId="Address">
+                <Form.Label className="FormLabel">
+                  Address<font COLOR="#ff0000">*</font>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="Address"
+                  required
+                 
+                ></Form.Control>
+              </Form.Group>
+                                               
+                                                </Col>
+                                                <Col><Form.Group controlId="Name">
+                <Form.Label className="FormLabel">Name <font COLOR="#ff0000">*</font></Form.Label>
+                <Form.Control
+                  type="text"
+                 required
+                //   defaultValue={this.props.AssignmentTopic}
+                //   dir="RTL"
+                  name="Name"
+                ></Form.Control>
+              </Form.Group>
+                                                </Col>
+                                                {/* <Col> <Form.Group controlId="Amount">
+                <Form.Label className="FormLabel">
+                  Amount
+                </Form.Label>
+                <Form.Control
+                  type="float"
+                  name="Amount"
+                  
+                 
+                ></Form.Control>
+              </Form.Group>
+                                                </Col> */}
+                                                <Col sm="2"><Button style={{marginTop: '33px'}} id="AccountBookDialogPayBtn"  class="text-button hover:text-white text-sm font-medium px-3 py-1 rounded hover:bg-gray-700 transition-colors border border-button" formAction="submitBook" type='submit' >
+                                        <Plus/>
+                                    </Button></Col>
+                                    <Col sm="2"><Button style={{marginTop: '33px'}} id="AccountBookDialogXCancelBtn"  class="text-button hover:text-white text-sm font-medium px-3 py-1 rounded hover:bg-gray-700 transition-colors border border-button" onClick={()=>setIsFormVisible("false")} >
+                                        <X/>
+                                    </Button></Col>
+                                            </Row>
+                                            <br></br>
+                                            <Row sm={2} >
+                                    {/* <Col lg='9'>
+                                    
+                                    </Col> */}
+                                {/* </div> */}
+                            {/* </div> */}
+                            
+                       </Row>
+                                          </Form> : <div class=" flex-1 min-w-0">
+                              <button onClick={()=>setIsFormVisible("true")}><h3 class="font-medium text-gray-400 group-hover:text-secondary" >Add New Address</h3></button>
+                            </div> }     
+                            
                           </td>
                           <td></td>
                         </tr>
@@ -161,52 +221,17 @@ function AccountBookDialog(props) {
                       <tbody></tbody>
                     )}
                 </Table>
-              </div>
-            </div>
-          </div>
+                                    </div>
+                                </div>
+                            </div>
 
-
-
-          <Form style={{ color: 'white' }} onSubmit={handleSubmit}>
-            <Row>
-              <Col>
-                <Form.Group controlId="Address">
-                  <Form.Label className="FormLabel">
-                    Address<font COLOR="#ff0000">*</font>
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="Address"
-                    required
-
-                  ></Form.Control>
-                </Form.Group>
-
-              </Col>
-              <Col><Form.Group controlId="Name">
-                <Form.Label className="FormLabel">Name <font COLOR="#ff0000">*</font></Form.Label>
-                <Form.Control
-                  type="text"
-                  required
-                  //   defaultValue={this.props.AssignmentTopic}
-                  //   dir="RTL"
-                  name="Name"
-                ></Form.Control>
-              </Form.Group>
-              </Col>
-              <Col><Button style={{ marginTop: '33px' }} id="AccountBookDialogPayBtn" class="text-secondary hover:text-white text-sm font-medium px-3 py-1 rounded hover:bg-gray-700 transition-colors border border-secondary" formAction="submitBook" type='submit' >
-                <Plus />
-              </Button></Col>
-            </Row>
-            <br></br>
-            <Row sm={2} >
-
-            </Row>
-          </Form>
-        </div>
-
-
-
+                            
+                                          
+                                     
+                                          </div>
+            
+                                    
+       
       </dialog>
     </div>
   );
