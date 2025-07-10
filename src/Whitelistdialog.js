53f245c993d351797223407d618f1c0ed1c1fd9d
mariokassab-emcrey <mario.kassab@emcrey.com>
2025-07-10 15:59:04 +0300

    testing the whitelist crud

--- src/Whitelistdialog.js
+++ src/Whitelistdialog.js
@@ -10,10 +10,12 @@ function WhitelistDialog(props) {
   const dialogRef = useRef(null);
   const [Whitelist, setWhitelist] = useState([]);
   const [IsFormVisible,setIsFormVisible] = useState(["false"]);
-  const [CurrentID,setCurrentID] = useState([""]);
+  const [canReceiveFlags,setCanReceiveFlags] = useState([""]);
  const [IsUpdate,setIsUpdate] = useState(["false"]);
  const [Address,setAddress] = useState([""]);
- const [Name,setName] = useState([""]);
+ const [canSendFlags,setCanSendFlags] = useState([""]);
+ const [isBlacklistedFlags,setIsBlacklistedFlags] = useState([""]);
+ 
 
   const openDialog = () => {
   
@@ -35,19 +37,38 @@ async function handleSubmit(event) {
   };
   if(IsUpdate==="true"){
     
-    
-  try {
-    console.log("CurrentID"+CurrentID)
-    const docRef = doc(db, "Whitelist", CurrentID);
-  await updateDoc(docRef, newAccount);
-       await fetchMenu()
-       setIsUpdate("false")
-       setIsFormVisible("false")
-       alert("List Updated successfully")
-  }catch (error) {
-      console.error("Error Updating: ", error);
-      alert("Failed Update the Whitelist.");
-    }
+    //call marie on update
+     const contractAddress = '0x715df9a13088ef923120f3355aa27441f9155614';
+  // const provider = new ethers.JsonRpcProvider('https://fluent-chaotic-lake.ethereum-sepolia.quiknode.pro/b989ab5dc846e9bfab7637b37a5b36f0bcd11fb5/');
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+  let flag = 0x00001;
+  if(event.target.isBlacklistedFlags.value)
+    flag = 0x00004;
+  else{
+    if(event.target.canReceiveFlags.value)
+       flag = 0x00002;
+      else{
+        if(event.target.canSendFlags.value){
+          flag = 0x00003;
+        }
+      }
+  }
+
+  const tx = await contract.addPermissions(event.target.Address.value,flag);
+  // try {
+  //   console.log("CurrentID"+CurrentID)
+  //   const docRef = doc(db, "Whitelist", CurrentID);
+  // await updateDoc(docRef, newAccount);
+  //      await fetchMenu()
+  //      setIsUpdate("false")
+  //      setIsFormVisible("false")
+  //      alert("List Updated successfully")
+  // }catch (error) {
+  //     console.error("Error Updating: ", error);
+  //     alert("Failed Update the Whitelist.");
+  //   }
   }
   else{
   
@@ -85,15 +106,43 @@ async function handleSubmit(event) {
 
 }
  const fetchMenu = async () => {
-      const menuCollection = await getDocs(collection(db, "Whitelist"));
-       console.log("menuCollection.docs"+menuCollection.docs[0].id)
-      const Addresses = menuCollection.docs.map((doc) => ({
-       ID:doc.id,
-        Address: doc.data().Address,
-        Name: doc.data().Name,
-      }));
-     
-      setWhitelist(Addresses);
+  const contractAddress = '0x715df9a13088ef923120f3355aa27441f9155614';
+  // const provider = new ethers.JsonRpcProvider('https://fluent-chaotic-lake.ethereum-sepolia.quiknode.pro/b989ab5dc846e9bfab7637b37a5b36f0bcd11fb5/');
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+  const menuCollection = await contract.getWhitelistedWithPermissions();
+  console.log("menuCollection"+menuCollection)
+    console.log("menuCollection0"+menuCollection[0])
+    console.log("menuCollection1"+menuCollection[1])
+    console.log("menuCollection2"+menuCollection[2])
+    console.log("menuCollection3"+menuCollection[3])
+      // const menuCollection = await getDocs(collection(db, "Whitelist"));
+      //  console.log("menuCollection.docs"+menuCollection.docs[0].id)
+    // const Addresses  {
+      
+    //     Address: menuCollection[0],
+    //     canSendFlags: menuCollection.[1],
+    //     canReceiveFlags:menuCollection.[2],
+    //     isBlacklistedFlags:menuCollection.[3],
+    //   }
+      const Addresses = () => ({
+      
+        Address: menuCollection[0],
+        canSendFlags: menuCollection[1],
+        canReceiveFlags:menuCollection[2],
+        isBlacklistedFlags:menuCollection[3],
+      });
+     const whitelistAddresses = menuCollection[0].map((collection,index)=>(
+      {
+        Address:collection,
+        canSendFlags:menuCollection[1][index],
+        canReceiveFlags:menuCollection[2][index],
+        isBlacklistedFlags:menuCollection[3][index],
+      }
+     ))
+     console.log("whitelistAddresses"+whitelistAddresses[2].canReceiveFlags)
+      setWhitelist(whitelistAddresses);
     };
 useEffect(() => {
    
@@ -125,6 +174,28 @@ useEffect(() => {
                                         <Table className="mt-4" hover size="sm" style={{ color: 'white' }}>
                   <thead>
                     <tr>
+                      <td></td>
+                       <td>
+                                
+                                   Address
+                               
+                              </td>
+                               <td>
+                              
+                                   Send
+                               
+                              </td>
+                               <td>
+                             Recieve
+                                   
+                                 
+                              </td>
+                               <td>
+                              BlackListed
+                                   
+                                
+                              </td>
+                              
                     </tr>
                   </thead>
                   {Whitelist.length !== 0
@@ -140,19 +211,38 @@ useEffect(() => {
                                   </div>
                                 </div>
                               </td>
-
                               <td className="td-home">
                                 <div class="flex flex-col">
-                                  <p class="text-lg font-bold">{account.Name}</p>
+                                  <p class="text-lg font-bold">{props.truncateEthAddress(account.Address)}</p>
                                   <div class="display: flex gap-1 text-gray-300">
-                                    {props.truncateEthAddress(account.Address)}
-                                    <button class="focus:outline-none" onClick={() => props.copyTextToClipboard(account.Address)} >
-                                      <Copy size={12} />
-                                    </button>
                                   </div>
                                 </div>
                               </td>
-
+                              <td className="td-home">
+                                <div class="flex flex-col">
+                                  <p class="text-lg font-bold">{account.canSendFlags.toString()}</p>
+                                  <div class="display: flex gap-1 text-gray-300">
+                                  </div>
+                                </div>
+                              </td>
+                              
+                               <td className="td-home">
+                                <div class="flex flex-col">
+                                  <p class="text-lg font-bold">{account.canReceiveFlags.toString()}</p>
+                                  <div class="display: flex gap-1 text-gray-300">
+                                    
+                                  </div>
+                                </div>
+                              </td>
+                               
+ <td className="td-home">
+                                <div class="flex flex-col">
+                                  <p class="text-lg font-bold">{account.isBlacklistedFlags.toString()}</p>
+                                  <div class="display: flex gap-1 text-gray-300">
+                                   
+                                  </div>
+                                </div>
+                              </td>
                               <td style={{ width: '200px' }}>
                                 <Row style={{ paddingLeft: '25px' }}>
 
@@ -169,18 +259,20 @@ useEffect(() => {
                                     :(<div/>)}
 
                                   <div style={{ width: '10px', height: '10px' }}></div>
-                                  <Trash2 class="text-gray-300 hover:text-secondary p-1 rounded-full hover:bg-gray-700 transition-colors"
+                                  {/* <Trash2 class="text-gray-300 hover:text-secondary p-1 rounded-full hover:bg-gray-700 transition-colors"
                                     onClick={async () => {
                                       await deleteDoc(doc(db, "Whitelist", account.ID));
                                       setWhitelist(Whitelist.filter((item) => item.ID !== account.ID));
                                     }}>
-                                  </Trash2>
+                                  </Trash2> */}
                                   <EditIcon class="text-gray-300 hover:text-secondary p-1 rounded-full hover:bg-gray-700 transition-colors"
                                    onClick={ () => {setIsUpdate("true")
                                     setIsFormVisible("true")
                                     setAddress(account.Address)
-                                    setName(account.Name)
-                                    setCurrentID(account.ID)
+                                    setCanSendFlags(account.canSendFlags)
+                                    setCanReceiveFlags(account.canReceiveFlags)
+                                    setIsBlacklistedFlags(account.isBlacklistedFlags)
+                                    
                                     
                                      
                                     }}>
@@ -195,7 +287,7 @@ useEffect(() => {
                           <td>
                             {IsFormVisible === "true" ? null : <div class="flex items-center gap-4 p-1 hover:bg-gray-700 rounded">
                               <div class="group w-10 h-10 rounded-full border-2 border-dashed border-gray-500 flex items-center justify-center hover:border-secondary" >
-                               <button> <Plus onClick={()=>{setIsFormVisible("true");setIsUpdate('false'); setAddress('');setName('');setCurrentID('')}} class="w-5 h-5 text-gray-500 group-hover:text-secondary" /></button>
+                               <button> <Plus onClick={()=>{setIsFormVisible("true");setIsUpdate('false'); setAddress('');setCanReceiveFlags('');setCanSendFlags('');setIsBlacklistedFlags('')}} class="w-5 h-5 text-gray-500 group-hover:text-secondary" /></button>
                               </div>
                             </div>}
                             
@@ -218,12 +310,36 @@ useEffect(() => {
               </Form.Group>
                                                
                                                 </Col>
-                                                <Col><Form.Group controlId="Name">
-                <Form.Label className="FormLabel">Name <font COLOR="#ff0000">*</font></Form.Label>
+                                                <Col><Form.Group controlId="canSendFlags">
+                <Form.Label className="FormLabel">canSendFlags <font COLOR="#ff0000">*</font></Form.Label>
                 <Form.Control
                   type="text"
                  required
-                 defaultValue={Name}
+                 defaultValue={canSendFlags}
+                //   defaultValue={this.props.AssignmentTopic}
+                //   dir="RTL"
+                  name="Name"
+                ></Form.Control>
+              </Form.Group>
+                                                </Col>
+                                                <Col><Form.Group controlId="canReceiveFlags">
+                <Form.Label className="FormLabel">canReceiveFlags <font COLOR="#ff0000">*</font></Form.Label>
+                <Form.Control
+                  type="text"
+                 required
+                 defaultValue={canReceiveFlags}
+                //   defaultValue={this.props.AssignmentTopic}
+                //   dir="RTL"
+                  name="Name"
+                ></Form.Control>
+              </Form.Group>
+                                                </Col>
+                                                <Col><Form.Group controlId="isBlacklistedFlags">
+                <Form.Label className="FormLabel">isBlacklistedFlags <font COLOR="#ff0000">*</font></Form.Label>
+                <Form.Control
+                  type="text"
+                 required
+                 defaultValue={isBlacklistedFlags}
                 //   defaultValue={this.props.AssignmentTopic}
                 //   dir="RTL"
                   name="Name"
@@ -259,7 +375,7 @@ useEffect(() => {
                             
                        </Row>
                                           </Form> : <div class=" flex-1 min-w-0">
-                              <button onClick={()=>{setIsFormVisible("true"); setAddress('');setName('');setCurrentID('')}}><h3 class="font-medium text-gray-400 group-hover:text-secondary" >Add New Address</h3></button>
+                              <button onClick={()=>{setIsFormVisible("true");setIsUpdate('false'); setAddress('');setCanReceiveFlags('');setCanSendFlags('');setIsBlacklistedFlags('')}}><h3 class="font-medium text-gray-400 group-hover:text-secondary" >Add New Address</h3></button>
                             </div> }     
                             
                           </td>
